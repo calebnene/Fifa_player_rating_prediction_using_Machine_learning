e269cf4effa430b93203b7482d07c726ba81ea94
calebnene <123897323+calebnene@users.noreply.github.com>
2023-11-25 22:01:14 +0000

    Add files via upload

--- multipage_app/homepage.py
+++ multipage_app/homepage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 # Reading the training dataset
 goalkeepers = pd.read_csv("C:/Users/cnama/Desktop/IIPGH_project/goalkeepers.csv")
